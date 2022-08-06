813b6c2a6bbf8f8891aade55bf81f929856ab522
Lauren N. Liberda <lauren@selfisekai.rocks>
2022-08-06 23:52:51 +0200

    asar unpack, extra resources now working, fixes

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod utils;
 
 use asar::AsarWriter;
 use types::PackageJson;
-use utils::{gen_copy_list, get_globs_and_file_sets};
+use utils::{gen_copy_list, get_globs_and_file_sets, refilter_copy_list};
 
 use std::fs;
 use std::fs::File;
@@ -27,6 +27,25 @@ enum Args {
         output: Option<String>,
     },
 }
+
+const STANDARD_FILTERS: [&str; 16] = [
+    "!**/node_modules/.bin",
+    "!**/*.{md,rst,markdown,txt}",
+    "!**/{test,tests,__tests__,powered-test,example,examples,readme,README,Readme,changelog,CHANGELOG,Changelog}",
+    "!**/test.*",
+    "!**/*.test.*",
+    "!**/._*",
+    "!**/{.editorconfig,.DS_Store,.git,.svn,.hg,CVS,RCS,.gitattributes,.nvmrc,.nycrc,Makefile}",
+    "!**/{__pycache__,thumbs.db,.flowconfig,.idea,.vs,.vscode,.nyc_output,.docker-compose.yml}",
+    "!**/{.github,.gitlab,.gitlab-ci.yml,appveyor.yml,.travis.yml,circle.yml,.woodpecker.yml}",
+    "!**/{package-lock.json,yarn.lock}",
+    "!**/.{git,eslint,tslint,prettier,docker,npm,yarn}ignore",
+    "!**/.{prettier,eslint,jshint,jsdoc}rc",
+    "!**/{.prettierrc,webpack.config,.jshintrc,jsdoc,.eslintrc}{,.json,.js,.yml,yaml}",
+    "!**/{yarn,npm}-{debug,error}{,.log,.json}",
+    "!**/.{yarn,npm}-metadata,integrity",
+    "!**/*.{iml,o,hprof,orig,pyc,pyo,rbc,swp,csproj,sln,xproj,c,h,cc,cpp,hpp,lzz,gyp,ts}",
+];
 
 fn main() {
     let args = Args::parse();
@@ -52,15 +71,26 @@ fn main() {
 
             let current_dir = std::env::current_dir().unwrap();
 
-            let (global_globs, file_sets) = get_globs_and_file_sets(files);
+            let (mut asar_global_globs, asar_file_sets) = get_globs_and_file_sets(files.clone());
+            asar_global_globs = asar_global_globs
+                .into_iter()
+                .chain(
+                    ["/node_modules/**/*", "!/tasje_out"]
+                        .into_iter()
+                        .chain(STANDARD_FILTERS)
+                        .map(str::to_string),
+                )
+                .collect();
             let (extra_global_globs, extra_file_sets) = get_globs_and_file_sets(extra_res);
 
-            let copy_list = gen_copy_list(&current_dir, &global_globs, &file_sets);
+            let asar_copy_list = gen_copy_list(&current_dir, &asar_global_globs, &asar_file_sets);
+            let unpacked_copy_list = refilter_copy_list(&asar_copy_list, &asar_unpack);
             let extra_copy_list =
                 gen_copy_list(&current_dir, &extra_global_globs, &extra_file_sets);
 
             if verbose {
-                eprintln!("copy_list: {:#?}", copy_list);
+                eprintln!("asar_copy_list: {:#?}", asar_copy_list);
+                eprintln!("unpacked_copy_list: {:#?}", unpacked_copy_list);
                 eprintln!("extra_copy_list: {:#?}", extra_copy_list);
             }
 
@@ -73,9 +103,11 @@ fn main() {
                     .unwrap_or_else(|| "tasje_out".to_string())
             }));
             fs::create_dir_all(&output_dir).expect("create output_dir");
+            let unpacked_dir = output_dir.join("app.asar.unpacked");
 
+            // write files into the asar
             let mut asar = AsarWriter::new();
-            for (copy_source, copy_target) in copy_list {
+            for (copy_source, copy_target) in &asar_copy_list {
                 asar.write_file(
                     copy_target,
                     fs::read(copy_source).expect("reading source file"),
@@ -85,6 +117,23 @@ fn main() {
             }
             asar.finalize(File::create(output_dir.join("app.asar")).unwrap())
                 .unwrap();
+
+            // copy unpacked asar resources
+            for (copy_source, copy_target) in &unpacked_copy_list {
+                let target = unpacked_dir.join(copy_target);
+                fs::create_dir_all(target.parent().unwrap())
+                    .expect("creating unpacked dir structure");
+                fs::copy(copy_source, target).expect("copying unpacked file");
+            }
+
+            // copy extra resources
+            for (copy_source, copy_target) in &extra_copy_list {
+                let target = output_dir.join(copy_target);
+                fs::create_dir_all(target.parent().unwrap())
+                    .expect("creating extra resource dir structure");
+                fs::copy(copy_source, output_dir.join(copy_target))
+                    .expect("copying extra resource file");
+            }
         }
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,40 +1,19 @@
 use std::{
     collections::HashSet,
+    fmt::Debug,
     path::{Path, PathBuf},
 };
 
+use globset::{Glob, GlobSetBuilder};
 use globwalk::{FileType, GlobWalkerBuilder};
 
-use crate::types::FileSet;
+use crate::{types::FileSet, STANDARD_FILTERS};
 
 pub fn get_globs_and_file_sets(files: Vec<FileSet>) -> (Vec<String>, Vec<FileSet>) {
     let global_globs = files
         .iter()
         .filter(|set| set.to.is_none())
-        .map(|set| set.from.clone())
-        .chain(
-            [
-                "node_modules/**/*",
-                "!**/node_modules/.bin",
-                "!**/*.{md,rst,markdown,txt}",
-                "!**/{test,tests,__tests__,powered-test,example,examples,readme,README,Readme,changelog,CHANGELOG,Changelog}",
-                "!**/test.*",
-                "!**/*.test.*",
-                "!**/._*",
-                "!**/{.editorconfig,.DS_Store,.git,.svn,.hg,CVS,RCS,.gitattributes,.nvmrc,.nycrc,Makefile}",
-                "!**/{__pycache__,thumbs.db,.flowconfig,.idea,.vs,.vscode,.nyc_output,.docker-compose.yml}",
-                "!**/{.github,.gitlab,.gitlab-ci.yml,appveyor.yml,.travis.yml,circle.yml,.woodpecker.yml}",
-                "!**/{package-lock.json,yarn.lock}",
-                "!**/.{git,eslint,tslint,prettier,docker,npm,yarn}ignore",
-                "!**/.{prettier,eslint,jshint,jsdoc}rc",
-                "!**/{.prettierrc,webpack,.jshintrc,jsdoc,.eslintrc}{,.json,.js}",
-                "!**/{yarn,npm}-{debug,error}{,.log,.json}",
-                "!**/.{yarn,npm}-metadata,integrity",
-                "!**/*.{iml,o,hprof,orig,pyc,pyo,rbc,swp,csproj,sln,xproj,c,h,cc,cpp,hpp,lzz,gyp,ts}",
-            ]
-            .into_iter()
-            .map(str::to_string),
-        )
+        .map(|set| "/".to_owned() + &set.from.clone())
         .collect();
 
     let file_sets = files
@@ -54,6 +33,11 @@ pub fn get_globs_and_file_sets(files: Vec<FileSet>) -> (Vec<String>, Vec<FileSet
                         .collect(),
                 );
             }
+            set.filter = set.filter.map(|f| {
+                f.into_iter()
+                    .chain(STANDARD_FILTERS.into_iter().map(str::to_string))
+                    .collect()
+            });
             set
         })
         .collect();
@@ -69,7 +53,6 @@ pub fn gen_copy_list<P: AsRef<Path>, S: AsRef<str>>(
     let mut copy_list: HashSet<(PathBuf, PathBuf)> = HashSet::new();
 
     for dir_entry in GlobWalkerBuilder::from_patterns(&base_dir, &global_globs)
-        .follow_links(true)
         .file_type(FileType::FILE)
         .build()
         .unwrap()
@@ -87,7 +70,6 @@ pub fn gen_copy_list<P: AsRef<Path>, S: AsRef<str>>(
         let target_dir = PathBuf::from(file_set.to.clone().unwrap_or_default());
         for dir_entry in
             GlobWalkerBuilder::from_patterns(&set_base_dir, file_set.filter.as_ref().unwrap())
-                .follow_links(true)
                 .file_type(FileType::FILE)
                 .build()
                 .unwrap()
@@ -96,10 +78,52 @@ pub fn gen_copy_list<P: AsRef<Path>, S: AsRef<str>>(
             let file_path = dir_entry.path();
             copy_list.insert((
                 file_path.to_path_buf(),
-                target_dir.join(file_path.strip_prefix(&base_dir).unwrap()),
+                target_dir.join(file_path.strip_prefix(&set_base_dir).unwrap()),
             ));
         }
     }
+
+    return copy_list;
+}
+
+pub fn refilter_copy_list<S: AsRef<str> + Debug>(
+    original_copy_list: &HashSet<(PathBuf, PathBuf)>,
+    new_globs: &[S],
+) -> HashSet<(PathBuf, PathBuf)> {
+    let mut set_b = GlobSetBuilder::new();
+    let set_negations: Vec<bool> = new_globs
+        .iter()
+        .map(|g| g.as_ref().starts_with('!'))
+        .collect();
+    for (i, g) in new_globs.iter().enumerate() {
+        let mut glob = g.as_ref();
+        if *set_negations.get(i).unwrap() {
+            // strip negation
+            glob = &glob[1..];
+        }
+        set_b.add(Glob::new(glob).unwrap());
+    }
+    let set = set_b.build().unwrap();
+    let mut copy_list = HashSet::new();
+
+    copy_list.extend(
+        original_copy_list
+            .clone()
+            .iter()
+            .filter(|(_, copy_target)| {
+                let matches = set.matches(copy_target);
+                // if no matches (including negations), filter out
+                if matches.len() == 0 {
+                    false
+                } else {
+                    matches.iter().any(|ms| {
+                        // if any negated glob matches, filter out
+                        !set_negations.get(ms.clone()).unwrap().to_owned()
+                    })
+                }
+            })
+            .map(|s| s.to_owned()),
+    );
 
     return copy_list;
 }
